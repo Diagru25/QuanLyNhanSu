25e9c9c07e8dd6dc326cad2d4b3609a5a2525458
Diagru25 <31183495+Diagru25@users.noreply.github.com>
2017-10-15 16:03:28 +0700

    pb

--- QuanLyNhanSu/QuanLyNhanSu/DAO/PhongBanDAO.cs
+++ QuanLyNhanSu/QuanLyNhanSu/DAO/PhongBanDAO.cs
@@ -9,6 +9,38 @@ namespace QuanLyNhanSu.DAO
 {
     public class PhongBanDAO
     {
-        
+        public static DataTable LoadPhongBan()
+        {
+            string query = "SELECT * FROM PHONGBAN";
+
+            DataTable data = SqlServerHelper.ExecuteQuery(query);
+
+            return data;
+        }
+        public static int InsertPhongBan(string tenpb)
+        {
+            string query = "INSERTPHONGBAN @TENPB";
+
+            return SqlServerHelper.ExecuteNonQuery(query, new object[] { tenpb });
+        }
+        public static int UpdatePhongBan(int idpb, string tenpb)
+        {
+            string query = "UPDATEPHONGBAN @IDPB , @TENPB ";
+
+            return SqlServerHelper.ExecuteNonQuery(query, new object[] { idpb, tenpb });
+        }
+        public static int DeletePhongBan(int idpb)
+        {
+            string query = "DELETEPHONGBAN @IDPB ";
+
+            try
+            {
+                return SqlServerHelper.ExecuteNonQuery(query, new object[] { idpb });
+            }
+            catch
+            {
+                return 0;
+            }
+        }
     }
 }
