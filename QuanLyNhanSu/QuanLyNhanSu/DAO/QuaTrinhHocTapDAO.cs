8ab41ab42d397d0896522508e1192f7920961cb9
Diagru25 <31183495+Diagru25@users.noreply.github.com>
2017-10-16 15:05:06 +0700

    laaatx

--- QuanLyNhanSu/QuanLyNhanSu/DAO/QuaTrinhHocTapDAO.cs
+++ QuanLyNhanSu/QuanLyNhanSu/DAO/QuaTrinhHocTapDAO.cs
@@ -9,6 +9,36 @@ namespace QuanLyNhanSu.DAO
 {
     public class QuaTrinhHocTapDAO
     {
-        
+        public static DataTable LoadQuaTrinhHocTap(int idnv)
+        {
+            string query = "select * from quatrinhhoctap where nhanvienid = " + idnv + "";
+
+            return SqlServerHelper.ExecuteQuery(query);
+        }
+        public static int InsertQTHT(int idnv, DateTime tgbatdau, DateTime tgketthuc, string noidung)
+        {
+            string query = "INSERTQUATRINHHOCTAP @IDNV , @TGBATDAU , @TGKETTHUC , @NOIDUNG ";
+
+            return SqlServerHelper.ExecuteNonQuery(query, new object[] { idnv, tgbatdau, tgketthuc, noidung });
+        }
+        public static int UpdateQTHT(int idQTHT, int idnv, DateTime tgbatdau, DateTime tgketthuc, string noidung)
+        {
+            string query = "UPDATEQUATRINHHOCTAP @IDQTHT , @IDNV , @TGBATDAU , @TGKETTHUC , @NOIDUNG ";
+
+            return SqlServerHelper.ExecuteNonQuery(query, new object[] { idQTHT, idnv, tgbatdau, tgketthuc, noidung });
+        }
+        public static int DeleteQTHT(int idQTHT)
+        {
+            string query = "DELETEQUATRINHHOCTAP @IDQTHT ";
+
+            try
+            {
+                return SqlServerHelper.ExecuteNonQuery(query, new object[] { idQTHT });
+            }
+            catch
+            {
+                return 0;
+            }
+        }
     }
 }
