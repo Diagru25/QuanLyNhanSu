4966bcb418e197be4c4a29f5ab1e79ecdc10c8e5
Diagru25 <31183495+Diagru25@users.noreply.github.com>
2017-10-16 19:09:15 +0700

    ts_tn

--- QuanLyNhanSu/QuanLyNhanSu/DAO/TaiSanDAO.cs
+++ QuanLyNhanSu/QuanLyNhanSu/DAO/TaiSanDAO.cs
@@ -9,6 +9,36 @@ namespace QuanLyNhanSu.DAO
 {
     public class TaiSanDAO
     {
-        
+        public static DataTable LoadTaiSan(int idnv)
+        {
+            string query = "select * from taisan where nhanvienid = " + idnv + "";
+
+            return SqlServerHelper.ExecuteQuery(query);
+        }
+        public static int InsertTaiSan(int idnv, string ten, string ghichu)
+        {
+            string query = "INSERTTAISAN @IDNV , @TEN , @GHICHU ";
+
+            return SqlServerHelper.ExecuteNonQuery(query, new object[] { idnv, ten, ghichu });
+        }
+        public static int UpdateTaiSan(int idtaisan, int idnv, string ten, string ghichu)
+        {
+            string query = "UPDATETAISAN @IDTAISAN , @IDNV , @TEN , @GHICHU ";
+
+            return SqlServerHelper.ExecuteNonQuery(query, new object[] { idtaisan, idnv, ten, ghichu });
+        }
+        public static int DeleteTaiSan(int idtaisan)
+        {
+            string query = "DELETETAISAN @IDTAISAN ";
+
+            try
+            {
+                return SqlServerHelper.ExecuteNonQuery(query, new object[] { idtaisan });
+            }
+            catch
+            {
+                return 0;
+            }
+        }
     }
 }
