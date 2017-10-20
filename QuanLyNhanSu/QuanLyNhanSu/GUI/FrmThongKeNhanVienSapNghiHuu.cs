2a2e8f15ad4769ad1e7e00dfe79079f5ecabdbb4
albertanhduy <albertanhduy@gmail.com>
2017-10-20 22:11:50 +0700

    Nghỉ hưu

--- QuanLyNhanSu/QuanLyNhanSu/GUI/FrmThongKeNhanVienSapNghiHuu.cs
+++ QuanLyNhanSu/QuanLyNhanSu/GUI/FrmThongKeNhanVienSapNghiHuu.cs
@@ -13,6 +13,41 @@ namespace QuanLyNhanSu.GUI
 {
     public partial class FrmThongKeNhanVienSapNghiHuu : Form
     {
-       
+        public FrmThongKeNhanVienSapNghiHuu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThongKe_Click(object sender, EventArgs e)
+        {
+            if (rbtnNhanVienSapNghiHuu.Checked == true)
+                dgrThongKeSinhNhat.DataSource = ThongKeDAO.ThongKeNghiHuu(DateTime.Now);
+            else
+                dgrThongKeSinhNhat.DataSource = ThongKeDAO.ThongKeNVMoi(DateTime.Now);
+        }
+
+        private void btnXuat_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Excel |*.xls";
+            saveFileDialog1.Title = "Save an Excel File";
+            saveFileDialog1.ShowDialog();
+
+            string FileName = saveFileDialog1.FileName.ToString();
+            try
+            {
+                dgrThongKeSinhNhat.ExportToXls(FileName);
+                MessageBox.Show("Xuất file excel thành công");
+            }
+            catch
+            {
+                MessageBox.Show("Vui lòng đóng file cần ghi lại để quá trình ghi thành công");
+            }
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
